71f5d96d8fb1a68b517944e883d27a84d3fb26dd
Dmitry Ryazancev <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-02-18 15:49:42 +0300

    Local install script

---
+++ install-local.sh
@@ -0,0 +1,11 @@
+mkdir -p target &&
+cd target &&
+mkdir -p install-release-local &&
+cd install-release-local &&
+echo "configuring release version..." &&
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DLOCAL_INSTALL=ON ../.. &&
+echo "building release version..." &&
+cmake --build . &&
+echo "installing release version..." &&
+sudo cmake --install . &&
+echo "done!"
