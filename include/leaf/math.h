2669c2893027c3919849620ae445efaf06cf9838
whs31 <75864292+whs31@users.noreply.github.com>
2024-02-19 23:31:18 +0300

    V 0.3.3

--- include/leaf/math.h
+++ include/leaf/math.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <limits>
+#include <numbers>
 #include <leaf/global.h>
 #include <leaf/traits.h>
 
@@ -17,4 +18,22 @@ namespace leaf
 
   template<traits::AnyNumber T>
   [[nodiscard]] constexpr auto null(T num) -> bool { return eq(num, 0); }
+
+  template<traits::AnyNumber T>
+  [[nodiscard]] constexpr auto to_radians(T deg) -> T {
+    using std::numbers::pi_v;
+    return static_cast<T>(deg * pi_v<T> / 180.0);
+  }
+
+  template<traits::AnyNumber T>
+  [[nodiscard]] constexpr auto to_degrees(T rad) -> T {
+    using std::numbers::pi_v;
+    return static_cast<T>(rad * 180.0 / pi_v<T>);
+  }
+
+  template<traits::FloatingPoint T>
+  [[nodiscard]] constexpr auto logp(T base, T num) -> T {
+    using std::log;
+    return log(num) / log(base);
+  }
 }
