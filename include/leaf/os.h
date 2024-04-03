fc46c807e42f9000e7e964037d40f4b2dc028396
whs31 <75864292+whs31@users.noreply.github.com>
2024-04-03 15:51:51 +0300

    Added support of other operating systems

--- include/leaf/os.h
+++ include/leaf/os.h
@@ -12,15 +12,24 @@ namespace leaf::os
   enum class OperatingSystem
   {
     Windows,
-    Linux
+    Linux,
+    Darwin,
+    Android,
+    Unknown
   };
 
   [[nodiscard]] constexpr auto system() -> OperatingSystem
   {
     #ifdef _WIN32
     return OperatingSystem::Windows;
-    #else
+    #elif defined(__APPLE__) || defined(__MACH__) || defined(__MACOS__) || defined(__APPLE_CC__)
+    return OperatingSystem::Darwin;
+    #elif defined(__ANDROID__) || defined(__ANDROID) || defined(__ANDROID_API__)
+    return OperatingSystem::Android;
+    #elif defined (__linux__) || defined (__linux) || defined(linux) || defined(__gnu_linux__)
     return OperatingSystem::Linux;
+    #else
+    return OperatingSystem::Unknown;
     #endif
   }
 
@@ -28,7 +37,10 @@ namespace leaf::os
     switch(system())
     {
       case OperatingSystem::Windows: return '\\';
-      case OperatingSystem::Linux: return '/';
+      case OperatingSystem::Linux: [[fallthrough]];
+      case OperatingSystem::Darwin: [[fallthrough]];
+      case OperatingSystem::Android: [[fallthrough]];
+      case OperatingSystem::Unknown: return '/';
       default: unreachable();
     }
   }
@@ -39,6 +51,7 @@ namespace leaf::os
   auto swap_endian(T u) -> T
   {
     static_assert(numeric_limits<unsigned char>::digits == 8, "unsigned char is not 8 bits");
+
     union
     {
       T u;
