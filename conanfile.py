2b8cb7a1cad03abecd17ed7edd3a8629ed8d3ee9
spoo0k <mukhinva2@gmail.com>
2024-05-02 15:56:25 +0300

    Update CMakeLists.txt and conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.files import rmdir
 
 class LeafRecipe(ConanFile):
     name = "leaf"
-    version = "0.6.3"
+    version = "0.6.4"
     description = "Coreutils library for C++ (poor man's Google::Abseil)"
     author = "whs31 <whs31@github.io>"
     topics = ("logging", "coreutils", "utility")
