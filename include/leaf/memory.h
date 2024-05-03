e93aec1729d0c147c70f40e4530a2b3a303f2baa
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-05-03 14:20:49 +0300

    NonNullRelaxed type

--- include/leaf/memory.h
+++ include/leaf/memory.h
@@ -1,7 +1,13 @@
 #pragma once
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "google-explicit-constructor"
 
 #include <new>
 #include <memory>
+#include <iosfwd>
+#include <type_traits>
+#include <system_error>
+#include <leaf/assert.h>
 #include <leaf/traits.h>
 #include <leaf/utils/noexcept.h>
 
@@ -77,6 +83,152 @@ namespace leaf
   template <typename T>
   class NonNullRelaxed
   {
+   public:
     static_assert(detail::is_comparable_to_nullptr<T>::value, "T must be comparable to nullptr");
+
+    template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+    constexpr NonNullRelaxed(U&& u) noexcept(std::is_nothrow_move_constructible_v<T>)
+      : m_ptr(std::forward<U>(u))
+    {
+      assert_expects(this->m_ptr != nullptr);
+    };
+
+    template <typename = std::enable_if<not std::is_same_v<std::nullptr_t, T>>>
+    constexpr NonNullRelaxed(T u) noexcept(std::is_nothrow_move_constructible_v<T>)
+      : m_ptr(std::move(u))
+    {
+      assert_expects(this->m_ptr != nullptr);
+    }
+
+    template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+    constexpr NonNullRelaxed(NonNullRelaxed<U> const& other) noexcept(std::is_nothrow_move_constructible_v<T>)
+      : NonNullRelaxed(other.get())
+    {}
+
+    NonNullRelaxed(NonNullRelaxed const& other) = default;
+    NonNullRelaxed(std::nullptr_t) = delete;
+    auto operator=(NonNullRelaxed const& other) -> NonNullRelaxed& = default;
+    auto operator=(std::nullptr_t) -> NonNullRelaxed& = delete;
+    auto operator++() -> NonNullRelaxed& = delete;
+    auto operator++(int) -> NonNullRelaxed& = delete;
+    auto operator--() -> NonNullRelaxed& = delete;
+    auto operator--(int) -> NonNullRelaxed& = delete;
+    auto operator+=(std::ptrdiff_t) -> NonNullRelaxed& = delete;
+    auto operator-=(std::ptrdiff_t) -> NonNullRelaxed& = delete;
+    auto operator[](std::ptrdiff_t) const -> void = delete;
+
+    constexpr auto get() const noexcept(noexcept(detail::value_or_reference_return_t<T>(std::declval<T&>())))
+      -> detail::value_or_reference_return_t<T>
+    {
+      return this->m_ptr;
+    }
+
+    constexpr operator T() const { return this->get(); }
+    constexpr auto operator->() const -> decltype(auto) { return this->get(); }
+    constexpr auto operator*() const -> decltype(auto) { return *(this->get()); }
+
+   private:
+    T m_ptr;
+  };
+
+  template <typename T>
+  auto make_non_null_relaxed(T&& t) noexcept { return NonNullRelaxed<std::remove_cv_t<std::remove_reference_t<T>>>{std::forward<T>(t)}; }
+
+  template <typename T>
+  auto operator<<(std::ostream& os, NonNullRelaxed<T> const& ptr) -> std::ostream&
+  {
+    os << ptr.get();
+    return os;
+  }
+
+  template <typename T, class U>
+  auto operator==(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(lhs.get() == rhs.get())) -> decltype(lhs.get() == rhs.get())
+  {
+    return lhs.get() == rhs.get();
+  }
+
+  template <typename T, class U>
+  auto operator!=(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(lhs.get() != rhs.get())) -> decltype(lhs.get() != rhs.get())
+  {
+    return lhs.get() != rhs.get();
+  }
+
+  template <typename T, class U>
+  auto operator<(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(std::less<>{}(lhs.get(), rhs.get()))) -> decltype(std::less<>{}(lhs.get(), rhs.get()))
+  {
+    return std::less<>{}(lhs.get(), rhs.get());
+  }
+
+  template <typename T, class U>
+  auto operator<=(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(std::less_equal<>{}(lhs.get(), rhs.get()))) -> decltype(std::less_equal<>{}(lhs.get(), rhs.get()))
+  {
+    return std::less_equal<>{}(lhs.get(), rhs.get());
+  }
+
+  template <typename T, class U>
+  auto operator>(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(std::greater<>{}(lhs.get(), rhs.get()))) -> decltype(std::greater<>{}(lhs.get(), rhs.get()))
+  {
+    return std::greater<>{}(lhs.get(), rhs.get());
+  }
+
+  template <typename T, class U>
+  auto operator>=(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs)
+  noexcept(noexcept(std::greater_equal<>{}(lhs.get(), rhs.get()))) -> decltype(std::greater_equal<>{}(lhs.get(), rhs.get()))
+  {
+    return std::greater_equal<>{}(lhs.get(), rhs.get());
+  }
+
+  template <typename T, typename U>
+  auto operator-(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs) -> std::ptrdiff_t = delete;
+
+  template <typename T, typename U>
+  auto operator-(NonNullRelaxed<T> const& lhs, std::ptrdiff_t) -> NonNullRelaxed<U> = delete;
+
+  template <typename T, typename U>
+  auto operator+(NonNullRelaxed<T> const& lhs, NonNullRelaxed<U> const& rhs) -> std::ptrdiff_t = delete;
+
+  template <typename T, typename U>
+  auto operator+(NonNullRelaxed<T> const& lhs, std::ptrdiff_t) -> NonNullRelaxed<U> = delete;
+
+  template <typename T, typename U = decltype(std::declval<T const&>().get()), bool = std::is_default_constructible_v<std::hash<U>>>
+  struct NonNullRelaxedHash
+  {
+    auto operator()(NonNullRelaxed<T> const& ptr) const -> std::size_t
+    {
+      return std::hash<U>{}(ptr.get());
+    }
+  };
+
+  template <typename T, typename U>
+  struct NonNullRelaxedHash<T, U, false>
+  {
+    NonNullRelaxedHash() = delete;
+    NonNullRelaxedHash(NonNullRelaxedHash const&) = delete;
+    auto operator=(NonNullRelaxedHash const&) = delete;
   };
 }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-dcl58-cpp"
+namespace std
+{
+  template <typename T>
+  struct hash<leaf::NonNullRelaxed<T>> : leaf::NonNullRelaxedHash<leaf::NonNullRelaxed<T>> {};
+}
+#pragma clang diagnostic pop
+
+namespace leaf
+{
+  template <typename T>
+  class NonNull : public NonNullRelaxed<T>
+  {
+    
+  };
+}
+
+#pragma clang diagnostic pop
