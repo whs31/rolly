7c93e6ee53d4485b5a8914c8bf6d9aa001b0f2f5
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-05-12 18:19:00 +0300

    Deletion of old files

--- include/leaf/leaf.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <leaf/global.h>
-#include <leaf/casts.h>
--- src/c++/detail/rtti.cc
+++ src/c++/detail/rtti.cc
@@ -1,36 +1,39 @@
-#include <leaf/utils/rtti.h>
+#include <lf/detail/corefn.h>
+#include <lf/detail/os.h>
 
 #include <cstdlib>
 #include <memory>
-#ifdef __GNUG__
-#include <cxxabi.h>
+
+#ifdef LF_COMPILER_GNU
+# include <cxxabi.h>
 #else
-#include <windows.h>
-#include <DbgHelp.h>
-#pragma comment(lib, "dbghelp.lib")
+# include <windows.h>
+# include <DbgHelp.h>
+# pragma comment(lib, "dbghelp.lib")
 #endif
 
 using std::unique_ptr;
 using std::free;
 
-namespace leaf::utils {
-#ifdef __GNUG__
-  auto detail::demangle(const char* name) -> string
+namespace lf::rtti
+{
+  #ifdef LF_COMPILER_GNU
+  auto demangle(const char* name) -> std::string
   {
     auto status = -4;
     const unique_ptr<char, void (*)(void*)> res {
       abi::__cxa_demangle(name, nullptr, nullptr, &status), free};
     return status == 0 ? res.get() : name;
   }
-#else
-  auto detail::demangle(const char* name) -> string
+  #else
+  auto detail::demangle(const char* name) -> std::string
   {
     constexpr auto max_name_length = DWORD(1024);
     char undecorated[max_name_length];
 
-    if (UnDecorateSymbolName(name, undecorated, max_name_length, UNDNAME_COMPLETE))
+    if(UnDecorateSymbolName(name, undecorated, max_name_length, UNDNAME_COMPLETE))
       return std::string(undecorated);
     return std::string(name);
   }
-#endif
-} // namespace leaf::utils
+  #endif
+}
