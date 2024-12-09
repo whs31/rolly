75a13403d0f7bebf845ec0b0b5be2c80641da67d
whs31 <whs31x@gmail.com>
2024-12-09 15:32:57 +0300

    VERSION: 2.1.45
    String utility for managing raw c strings and scope_guard class

--- include/rolly/functional.h
+++ include/rolly/functional.h
@@ -2,4 +2,5 @@
 
 #include "functional/lazy.h"
 #include "functional/overload.h"
+#include "functional/scope_guard.h"
 #include "functional/signals.h"
--- include/rolly/memory.h
+++ include/rolly/memory.h
@@ -2,6 +2,6 @@
 
 #include "memory/box.h"
 #include "memory/deleter.h"
+#include "memory/observer_ptr.h"
 #include "memory/owner.h"
 #include "memory/propagate_const.h"
-#include "memory/observer_ptr.h"
