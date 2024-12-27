fd34d6cac2aefdbca65b176340a6dc047b812f85
whs31 <riazantsev.dl@edu.spbstu.ru>
2024-12-27 20:43:07 +0300

    MAINTENANCE: deleted obsolete files

--- src/oslayer/linux/library.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../library.h"
-
-#ifndef ROLLY_OS_WINDOWS
-namespace rolly::oslayer::linux {
-  [[nodiscard]] void* lib_resolve(std::atomic<void*>& handle, std::string_view symbol);
-}  // namespace rolly::oslayer::linux
-#endif
